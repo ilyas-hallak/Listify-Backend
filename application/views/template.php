1b71846c003c291eaf0d9e7fcd613b2cb741f2f9
Ilyas Hallakoglu <ilhallak@gmail.com>
2014-05-14 15:56:43 +0200

    ordner struktur + bootstrap installiert

---
+++ application/views/template.php
@@ -0,0 +1,5 @@
+<?php
+    $this->load->view('template/head', $title);
+    echo $content;
+	$this->load->view('template/footer');
+?>
---
+++ application/views/template/head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title><?=$title ?></title>
+	<style type="text/css">
+		div#wrapper {
+			text-align: center;
+		}
+		div#video {
+			width: 600px;
+			height: 500px;
+			margin: 0 auto;
+		}
+		div#tags {
+			margin-bottom: 10px;
+			margin-top: 10px;
+		
+			width: 600px;
+			margin: 0 auto;
+			
+		}
+	</style>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+</head>
+<body>
+	<div id="wrapper">
